9c80370f958559b25747e85aca27e89ca8bad7cd
lucasaledi <lucasaledi@gmail.com>
2023-05-22 21:38:31 +0200

    creditCard model

--- src/main/java/com/aledluca/ironExpress/models/CreditCard.java
+++ src/main/java/com/aledluca/ironExpress/models/CreditCard.java
@@ -1,4 +1,15 @@
 package com.aledluca.ironExpress.models;
 
+import jakarta.persistence.Entity;
+import jakarta.validation.constraints.*;
+
+@Entity
 public class CreditCard {
+    @NotNull(message = "Credit Card number cannot be null")
+    @Pattern(regexp = "[0-9]{16,18}", message = "Invalid card number")
+    private String cardNumber;
+    @Pattern(regexp = "[0-9]{2}/[0-9]{2,4}", message = "Invalid expire date. Format must be MM/YY or MM/YYYY")
+    private String expireDate;
+    @Pattern(regexp = "[0-9]{3}", message = "Invalid CVV. Must be numeric 3 digits length.")
+    private String cvv;
 }
